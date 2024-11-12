a95170a0df08c604ae494841e5746c7ed9f6e76c
mberenyi <miklos.berenyi.2002@gmail.com>
2024-11-12 21:32:30 +0100

    l

---
+++ 1-3.py
@@ -0,0 +1,39 @@
+# Input string
+
+L = False
+F = False
+R = False
+
+input_string = '|*|*|\n|-|*|\n|-|-|\n|>|<^>|\n'
+
+rows = input_string.strip().split('\n')
+result = []
+
+for row in rows:
+    columns = row.strip('|').split('|')
+    result.append(columns)
+transposed_result = list(zip(*result))
+transposed_result = [list(row) for row in transposed_result]
+for column in transposed_result:
+    if column[2] == '*':
+        if '<' in column[3]:
+            L = True
+        if '>' in column[3]:
+            R = True
+        if '^' in column[3]:
+            F = True
+    print(column)
+
+res = ''
+if L:
+    res += 'L'
+if F:
+    res += 'F'
+if R:
+    res += 'R'
+
+if not L and not F and not R:
+    res = 'STOP'
+
+print(res)
+
--- 3.py
+++ 3.py
@@ -23,18 +23,16 @@ def count_islands(matrix):
         for dr, dc in directions:
             dfs(row + dr, col + dc)
 
-    # Traverse each cell in the matrix
     for r in range(rows):
         for c in range(cols):
             if matrix[r][c] == '1' and (r, c) not in visited:
-                # Found an unvisited land cell, start a DFS from here
                 dfs(r, c)
                 island_count += 1
 
     return island_count
 
 # Test input
-matrix_string = '11101001\n11011011\n00100011\n11111111\n11111000\n11011101\n10011101\n11110110\n11111011\n01100011'
+matrix_string = '1011011\n0110000\n0000100\n1100001\n0110111\n0001101\n0001000'
 matrix = parse_string_to_matrix(matrix_string)
 
 # Print matrix rows for debugging
