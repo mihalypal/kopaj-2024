de9e069f7fc18770e9aa0a4438b29221a01878b3
balint0513 <balint0513@gmail.com>
2024-11-12 20:28:10 +0100

    jaj

--- Level1Task1.py
+++ Level1Task1.py
@@ -1,6 +1,7 @@
-def smallest_hole(dimensions):
+def smallest_hole(input_string):
+    dimensions = list(map(int, input_string.split(',')))
     sorted_dimensions = sorted(dimensions)
-    return sorted_dimensions[0], sorted_dimensions[1]
+    return f"{sorted_dimensions[0]},{sorted_dimensions[1]}"
 
-dimensions = 0,8,8
-smallest_hole(dimensions)
+input_string = "6,1,0"
+(smallest_hole(input_string))
