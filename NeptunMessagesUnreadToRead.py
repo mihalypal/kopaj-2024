55b4473205e7a84e709cd9bbe166494827963315
mihalypal <palijan.09@gmail.com>
2024-12-10 05:28:56 +0100

    message reader update

--- NeptunMessagesUnreadToRead.py
+++ NeptunMessagesUnreadToRead.py
@@ -2,7 +2,8 @@ from typing import List
 import time
 import requests
 
-post_ids_number = 0     # store the number of unread messages
+post_ids_number = 0  # store the number of unread messages
+
 
 # The following functions are used to get the access token, get the unread messages, mark the messages as read
 
@@ -29,17 +30,18 @@ def get_access_token(username: str, password: str) -> tuple:
 
 
 def get_unread_messages(access_token: str, cookie: str) -> List[str]:
-    first_row = 0           # The number(id) of the first message to get
-    last_row = 3000         # The number(id) of the last message to get
-                            # If you have more than 3000 messages, you can increase this number
-                            # The code will get the first 3000 messages -> [0, 3000], but you can change it if you want
+    first_row = 0  # The number(id) of the first message to get
+    last_row = 3000  # The number(id) of the last message to get
+    # If you have more than 3000 messages, you can increase this number
+    # The code will get the first 3000 messages -> [0, 3000], but you can change it if you want
     url = f'https://neptun.szte.hu/hallgato/api/Message/GetReceivedMessages?firstRow={first_row}&lastRow={last_row}&filterType=0'
     headers = {
         'Authorization': f'Bearer {access_token}',
         'Cookie': cookie
     }
     response = requests.get(url, headers=headers)
-    unread_messages = [message for message in response.json()['data']['receivedMessages'] if message['unreadedPostCount'] != 0]
+    unread_messages = [message for message in response.json()['data']['receivedMessages'] if
+                       message['unreadedPostCount'] != 0]
     unread_messages = [i['messageId'] for i in unread_messages]
     global post_ids_number
     post_ids_number = len(unread_messages)
@@ -72,7 +74,8 @@ def mark_messages_as_read(access_token: str, message_ids: List[str], cookie) -> 
         requests.post(url, json=data, headers=headers)
         print("Olvasatlan üzenetek száma:", post_ids_number)
         post_ids_number -= 1
-        time.sleep(0.2)     # You can change this number if you get connection error or something like that (the value is in seconds)
+        time.sleep(
+            0.2)  # You can change this number if you get connection error or something like that (the value is in seconds)
     print("Olvasatlan üzenet id-k száma:", post_ids_number)
     print("Olvasatlan üzenetek olvasottá tétele sikeres!")
 
@@ -112,7 +115,8 @@ def main(username: str = '', password: str = ''):
 # main()
 
 import tkinter as tk
-from tkinter import simpledialog
+from tkinter import CENTER
+
 
 def get_user_data():
     def on_submit():
@@ -123,18 +127,40 @@ def get_user_data():
         if res == 0:
             return
 
+    def on_enter(event):
+        on_submit()
+
     root = tk.Tk()
     root.title("Login")
+    root.geometry("300x150+300+120")
 
-    tk.Label(root, text="Username:").grid(row=0, column=0)
+    root.resizable(False, False)
+    root.attributes("-alpha", 0.9)
+
+    # Configure rows and columns for centering
+    root.grid_rowconfigure(0, weight=1)
+    root.grid_rowconfigure(1, weight=0)  # Sor közti távolság csökkentése
+    root.grid_rowconfigure(2, weight=0)
+    root.grid_rowconfigure(3, weight=0)
+    root.grid_rowconfigure(4, weight=1)  # Gomb alatti hely
+    root.grid_columnconfigure(0, weight=1)
+    root.grid_columnconfigure(1, weight=1)
+
+    # Username label and entry
+    tk.Label(root, text="Username:").grid(row=1, column=0, sticky="e", padx=5, pady=2)
     entry_username = tk.Entry(root)
-    entry_username.grid(row=0, column=1)
+    entry_username.grid(row=1, column=1, sticky="w", padx=5, pady=2)
 
-    tk.Label(root, text="Password:").grid(row=1, column=0)
+    # Password label and entry
+    tk.Label(root, text="Password:").grid(row=2, column=0, sticky="e", padx=5, pady=2)
     entry_password = tk.Entry(root, show="*")
-    entry_password.grid(row=1, column=1)
+    entry_password.grid(row=2, column=1, sticky="w", padx=5, pady=2)
 
-    tk.Button(root, text="Submit", command=on_submit).grid(row=2, columnspan=2)
+    # Bind Enter key to password field
+    entry_password.bind("<Return>", on_enter)
+
+    # Submit button
+    tk.Button(root, text="Submit", command=on_submit).grid(row=3, column=0, columnspan=2, pady=5)
 
     root.mainloop()
 
